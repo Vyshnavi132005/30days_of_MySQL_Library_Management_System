6585ecbdcc3c961f3451ebd87c1b3d29e6c8abae
Vyshnavi Srikakulam <vyshnavihoney08@gmail.com>
2026-05-30 11:49:01 +0530

    Add files via upload

---
+++ Day-01_Introduction of Data,Database/creating_databases.sql
@@ -0,0 +1,16 @@
+
+
+## 📄 day01.sql
+-- Day 01 Practice
+
+-- Show available databases
+SHOW DATABASES;
+
+-- Create a database
+CREATE DATABASE college_db;
+
+-- Select the database
+USE college_db;
+
+-- Delete the database
+DROP DATABASE college_db;
